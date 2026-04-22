8b73c726e2829889d5031ab2e22ce22b76bc4f72
Oscar Mauricio Muñoz Mejía <145808047+MasacradorM@users.noreply.github.com>
2026-04-22 10:33:46 -0500

    Add getters and setters for Product entity fields

--- Api-SpringBoot/src/main/java/com/sena/Api_SpringBoot/entity/Product.java
+++ Api-SpringBoot/src/main/java/com/sena/Api_SpringBoot/entity/Product.java
@@ -27,4 +27,44 @@ public class Product {
     private Integer stock;
 
     private String category;
+
+    public Long getId() {
+    return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
